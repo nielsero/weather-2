a132c1d52283da4a20946a327e8998e343f70530
Niels <niels.cpp@gmail.com>
2022-08-03 17:48:24 +0200

    Get weather data from city input

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import "./style.css"
-import getWeather from "./modules/getWeather"
 import bindEventListeners from "./modules/bindEventListeners"
 
 main()
 
-async function main() {
+function main() {
   console.log("[main]")
-  try {
-    const weather = await getWeather("maputo")
-    console.log(weather)
-  } catch (error) {
-    console.log(`[main] error: ${error.message}`)
-  }
   bindEventListeners()
 }
--- src/modules/bindEventListeners.js
+++ src/modules/bindEventListeners.js
@@ -1,3 +1,5 @@
+import getWeather from "./getWeather"
+
 function bindEventListeners() {
   console.log("[bindEventListeners]")
   const weatherForm = document.querySelector(".weather-form")
@@ -9,7 +11,14 @@ function handleWeatherFormSubmit(event) {
   event.preventDefault()
   const cityInput = event.target.querySelector(".city-input")
   const city = cityInput.value
-  console.log(city)
+
+  getWeather(city)
+    .then((weather) => {
+      console.log(weather)
+    })
+    .catch((error) => {
+      console.log(`[handleWeatherFormSubmit] error: ${error.message}`)
+    })
 }
 
 export default bindEventListeners
